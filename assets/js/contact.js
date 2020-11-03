715fbf987332f374bcd3b8bfcd8dd05df5a8bf13
damiandominella <damiandominella@gmail.com>
2020-11-03 15:09:04 +0100

    fixes

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,85 +1,113 @@
-$(document).ready(function(){
-    
-    (function($) {
-        "use strict";
+// -----------------------------------------------------------------
+// Constants
+// -----------------------------------------------------------------
+const submitFormURL =
+  "https://website-php-mailsend-dot-golee-infra.appspot.com/";
 
-    
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "type the correct answer -_-");
+// Base rules used in every form
+const baseRules = {
+  firstname: {
+    required: true,
+    minlength: 2,
+  },
+  lastname: {
+    required: true,
+    minlength: 2,
+  },
+  email: {
+    required: true,
+    email: true,
+  },
+  phone: {
+    required: true,
+    minlength: 9,
+  },
+  privacy: {
+    required: true,
+  },
+};
 
-    // validate contactForm form
-    $(function() {
-        $('#contactForm').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                subject: {
-                    required: true,
-                    minlength: 4
-                },
-                number: {
-                    required: true,
-                    minlength: 5
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
-                }
-            },
-            messages: {
-                name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
-                },
-                number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
-            }
-        })
-    })
-        
- })(jQuery)
-})
+// Base validation messages used in every form
+const baseValidationMsg = {
+  firstname: {
+    required: "Campo obbligatorio",
+    minlength: "Sono richiesti almeno 2 caratteri",
+  },
+  lastname: {
+    required: "Campo obbligatorio",
+    minlength: "Sono richiesti almeno 2 caratteri",
+  },
+  email: {
+    required: "Campo obbligatorio",
+  },
+  phone: {
+    required: "Campo obbligatorio",
+    minlenght: "Inserisci prefisso e numero (minimo 8 cifre)",
+  },
+  privacy: {
+    required: "Per inviare la richiesta il consenso Privacy è necessario",
+  },
+};
+
+// Compose form data
+function createFormData(form) {
+  var formData = new FormData();
+  for (item of $(form).serializeArray()) {
+    formData.append(item.name, item.value);
+  }
+
+  formData.append("templateId", "d-d08314b7c9c2414cb1567088c8964d56");
+
+  return formData;
+}
+
+// Handle response
+function handleResponse(success, formId) {
+  console.log(success);
+  if (!!success) {
+    $(`#${formId}`).hide();
+    $("#form-success").fadeIn();
+  } else {
+    $("#form-error").fadeIn();
+  }
+}
+
+// ---------------------------------------------------------------
+// Init document
+// ---------------------------------------------------------------
+$(document).ready(function () {
+  // ---------------------------------------------------------------
+  // Contact form
+  // ---------------------------------------------------------------
+  $("#contactFormSupport form").validate({
+    rules: {
+      ...baseRules,
+      message: {
+        required: true,
+        minlength: 20,
+      },
+    },
+    messages: {
+      ...baseValidationMsg,
+      message: {
+        required: "Campo obbligatorio",
+        minlength: "Sono richiesti almeno 20 caratteri",
+      },
+    },
+    submitHandler: function (form) {
+      try {
+        $.ajax({
+          url: submitFormURL,
+          data: createFormData(form),
+          processData: false,
+          contentType: false,
+          type: "POST",
+        }).done(function (res) {
+          handleResponse(true, "contactFormSupport");
+        });
+      } catch (err) {
+        handleResponse(false, "contactFormSupport");
+      }
+    },
+  });
+});
